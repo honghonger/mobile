2cd94f4d975c5769b8287d5d647b9c9e67600fe8
honghonger <1121101850@qq.com>
2020-01-11 20:13:51 +0800

    封装vaant组件按需引入

--- src/main.js
+++ src/main.js
@@ -3,8 +3,7 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import 'amfe-flexible'
-import { Button } from 'vant'
-Vue.use(Button)
+import './utils/regist.js'
 Vue.config.productionTip = false
 
 new Vue({
