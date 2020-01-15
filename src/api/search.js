28f40f54d85100b3d3a8bafba3f3a3c0409f68e9
honghonger <1121101850@qq.com>
2020-01-15 21:40:33 +0800

    完成联想搜索

--- src/api/search.js
+++ src/api/search.js
@@ -1,14 +1,15 @@
-
 import request from '@/utils/request'
 // 导出一个函数 组件中直接调用
 /**
  *
- * 封装文章列表请求
+ * 封装搜索内容模块
  */
-export function getArticles (params) {
+export function getSearch (q) {
   return request({
     method: 'GET',
-    url: '/app/v1_0/articles',
-    params
+    url: '/app/v1_0/suggestion',
+    params: {
+      q
+    }
   })
 }
--- src/views/search/index.vue
+++ src/views/search/index.vue
@@ -2,19 +2,24 @@
   <div class="search">
       <!-- 搜索框 -->
     <van-search
-        v-model="value"
+        v-model="searchText"
         placeholder="请输入搜索关键词"
         show-action
         shape="round"
         @search="onSearch"
+        @input="onSearchIpunt"
     >
        <div slot="action" @click="onSearch">搜索</div>
     </van-search>
     <!-- 搜索框 -->
     <!-- 联想建议表格 -->
     <van-cell-group>
-        <van-cell title="单元格"  icon="search"/>
-        <van-cell title="单元格"  icon="search"/>
+        <van-cell
+        :title="item"
+        icon="search"
+        v-for="item in searchs"
+        :key="item"
+        />
     </van-cell-group>
     <!-- 联想建议表格 -->
     <!-- 历史记录 -->
@@ -33,15 +38,29 @@
 </template>
 
 <script>
+import { getSearch } from '@/api/search'
+
 export default {
   name: 'searchPage',
   data () {
     return {
-      value: ''
+      searchText: '', // 用户输入的文本
+      searchs: []// 联想数据列表
     }
   },
   methods: {
-    onSearch () {}
+    onSearch () {},
+
+    async onSearchIpunt () {
+      // 非空校验
+      const searchText = this.searchText.trim()
+      if (!searchText) {
+        return
+      }
+      const res = await getSearch(this.searchText)
+      //   console.log(res)
+      this.searchs = res.data.data.options
+    }
   }
 }
 </script>
